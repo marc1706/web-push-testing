d03e9e9d0a6b0991b0ac765243a80cc9d897d128
Marc Alexander <admin@m-a-styles.de>
2021-12-25 11:34:50 +0100

    Add file for starting testing server

---
+++ src/bin/server.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+
+/**
+ *
+ * This file is part of the web-push-testing package.
+ *
+ * @copyright (c) Marc Alexander <https://www.m-a-styles.de>
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ *
+ */
+
+const testingServer = require('../server.js');
+
+const serverPort = JSON.parse(process.argv[2]);
+
+const server = new testingServer(serverPort);
+server.startServer();
