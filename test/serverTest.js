de41324badae859e1b977b9ff4c8f61d069b916c
Marc Alexander <admin@m-a-styles.de>
2022-01-05 21:52:20 +0100

    Add tests for getting notifications & subscribing via server

--- test/serverTest.js
+++ test/serverTest.js
@@ -59,7 +59,7 @@ describe('Push Server tests', function() {
         console.error = originalError;
     };
 
-    it('should be able to start server via constructor', function() {
+    it('should be able to start server via constructor', async function() {
         const model = new pushApiModel();
         const port = 8990;
         const server = new testingServer(model, port);
@@ -75,7 +75,7 @@ describe('Push Server tests', function() {
             });
         };
 
-        getStatus().then((response) => {
+        await getStatus().then((response) => {
             response.status.should.equal(200);
             consoleLogs.length.should.equal(1);
             consoleLogs[0].should.match(/Server running/);
@@ -94,5 +94,157 @@ describe('Push Server tests', function() {
         server.startServer();
         consoleErrors.length.should.equal(1);
         consoleErrors[0].should.match(/Invalid port supplied/);
+    });
+
+    describe('Get notifications from server', function() {
+        const input = [
+            {
+                description: '0 messages from server',
+                messages: [],
+                validClientHash: true,
+            },
+            {
+                description: '1 message from server',
+                messages: ['testMessage1'],
+                validClientHash: true,
+            },
+            {
+                description: '2 messages from server',
+                messages: ['testMessage1', 'testMessage2'],
+                validClientHash: true,
+            },
+            {
+                description: '0 messages from server',
+                messages: [],
+                validClientHash: false,
+                error: {
+                    status: 400,
+                    message: 'Client not subscribed',
+                }
+            },
+        ];
+
+        input.forEach(({description, messages, validClientHash, error}) => {
+            it(description, async function() {
+                const model = new pushApiModel();
+                const testClientHash = 'testHash';
+                model.subscriptions[testClientHash] = {some: 'data'};
+                model.messages[testClientHash] = messages;
+                const port = 8990;
+
+                const server = new testingServer(model, port);
+                startLogging();
+                server.startServer();
+
+                await fetch('http://localhost:' + port + '/status', {
+                    method: 'POST',
+                }).then((response) => {
+                    response.status.should.equal(200);
+                    consoleLogs.length.should.equal(1);
+                    consoleLogs[0].should.match(/Server running/);
+                })
+                .then(async () => {
+                    return await fetch('http://localhost:' + port + '/get-notifications', {
+                        method: 'POST',
+                        body: JSON.stringify({
+                            clientHash: validClientHash ? testClientHash : 'wrong',
+                        }),
+                        headers: {'Content-Type': 'application/json'}
+                    });
+                })
+                .then(async (response) => {
+                    endLogging();
+                    server._server.close();
+                    if (validClientHash) {
+                        response.status.should.equal(200);
+                        const responseBody = await response.json();
+                        assert.hasAnyKeys(responseBody, ['data']);
+                        assert.hasAnyKeys(responseBody.data, ['messages']);
+                        responseBody.data.messages.length.should.equal(messages.length);
+                        for (let i = 0; i < responseBody.data.messages.length; i++) {
+                            responseBody.data.messages[i].should.equal(messages[i]);
+                        }
+                    } else {
+                        response.status.should.equal(error.status);
+                        const responseBody = await response.json();
+                        assert.hasAnyKeys(responseBody, ['error']);
+                        assert.hasAnyKeys(responseBody.error, ['message']);
+                        responseBody.error.message.should.equal(error.message);
+                    }
+                });
+            });
+        });
+    });
+
+    describe('Subscribe via server', () => {
+        const input = [
+            {
+                description: 'Valid subscription with empty options',
+                options: {},
+                expectedReturn: true,
+            },
+            {
+                description: 'Valid subscription with only userVisibleOnly option',
+                options: {userVisibleOnly: 'true'},
+                expectedReturn: true,
+            },
+            {
+                description: 'Valid subscription with userVisibleOnly & applicationServerKey',
+                options: {userVisibleOnly: 'true', applicationServerKey: 'BLFs1fhFLaLQ1VUOsQ0gqysdZUigBkR729fgFLO99fTNRr9BJPY02JyOSXVqoPOYkG-nzNu83EEzpmeJgphXCoM'},
+                expectedReturn: true,
+            },
+            {
+                description: 'Invalid application server key',
+                options: {'applicationServerKey': 'nope'},
+                expectedReturn: false,
+                expectedError: {
+                    type: Error,
+                    match: /Parameter applicationServerKey does not seem to be a valid VAPID key/,
+                },
+            },
+            {
+                description: 'Invalid userVisibleOnly',
+                options: {userVisibleOnly: 'nope', applicationServerKey: 'BLFs1fhFLaLQ1VUOsQ0gqysdZUigBkR729fgFLO99fTNRr9BJPY02JyOSXVqoPOYkG-nzNu83EEzpmeJgphXCoM'},
+                expectedReturn: false,
+                expectedError: {
+                    type: RangeError,
+                    match: /Parameter userVisibleOnly is not of type boolean/,
+                },
+            },
+        ];
+
+        input.forEach(({description, options, expectedReturn, expectedError}) => {
+            it(description, async function() {
+                const model = new pushApiModel();
+                const port = 8990;
+
+                const server = new testingServer(model, port);
+                startLogging();
+                server.startServer();
+
+                await fetch('http://localhost:' + port + '/status', {
+                    method: 'POST',
+                }).then((response) => {
+                    response.status.should.equal(200);
+                    consoleLogs.length.should.equal(1);
+                    consoleLogs[0].should.match(/Server running/);
+                })
+                .then(async () => {
+                    return await fetch('http://localhost:' + port + '/subscribe', {
+                        method: 'POST',
+                        body: JSON.stringify(options),
+                        headers: {'Content-Type': 'application/json'}
+                    });
+                })
+                .then((response) => {
+                    endLogging();
+                    server._server.close();
+                    response.status.should.equal(expectedReturn ? 200 : 400);
+                })
+                .then(() => {
+
+                });
+            });
+        });
     });
 });
